9c5dbd462954108f37511ff702b55113b3be269a
savel <savelikor@gmail.com>
2020-12-27 00:03:57 +0500

    LEETCODE-1022 Refactored generator.py

--- generator/generator.py
+++ generator/generator.py
@@ -1,9 +1,16 @@
-import sys
+#import sys
 
 class LeetCodeSource:
     def __init__(self, title, link, code):
         self.title = title.rstrip('\n').split('. ')[1]
         self.link = link.rstrip('\n')
+        link_inter = []
+        i = -2
+        while self.link[i] != '/':
+            link_inter += self.link[i]
+            i -= 1
+        link_inter.reverse()
+        self.link_inter = ''.join(link_inter)
         self.code = code
 
     def get_md_formatted_code(self):
@@ -13,7 +20,7 @@ class LeetCodeSource:
         return '## {}'.format(self.title)
 
     def get_md_solution_link(self):
-        return '+ [{}](#{})'.format(self.title, self.link[-6:-1])
+        return '+ [{}](#{})'.format(self.title, self.link_inter)
 
     def get_md_formatted_solution(self):
         return (self.get_md_solution_link(), '{}\n\n{}\n\n{}'.format(self.get_md_title(), self.link, self.get_md_formatted_code()))
@@ -35,19 +42,25 @@ def read_all_file(file_name):
     file.close()
     return result
 
-def merge_solutions(old_solution, new_solution, emerged_link):
+def merge_solutions(old_solution, new_solution, emerged_link, header_title):
     old_splitted = old_solution.split('\n\n')
     if len(old_splitted) == 1:
-        return '#Math\n\n{}'.format('\n\n'.join(map(lambda x: x, [emerged_link, new_solution])))
+        return '# {}\n\n{}'.format(header_title, '\n\n'.join(map(lambda x: x, [emerged_link, new_solution])))
     return '{}\n{}\n\n{}\n\n{}'.format('\n\n'.join(old_splitted[:2]), emerged_link, '\n\n'.join(old_splitted[2:]), new_solution)
 
 def main(src, dst):
     in_text = read_all_lines_from_file(src)
+    i = -4
+    preheader = []
+    while dst[i] != '\\':
+        preheader.insert(0, dst[i])
+        i -= 1
+    header = ''.join(preheader).capitalize()
     x = LeetCodeSource(in_text[0], in_text[1], in_text[2:])
     emerged_links, new_solutions = x.get_md_formatted_solution()
     old_solutions = read_all_file(dst)
-    result = merge_solutions(old_solutions, new_solutions, emerged_links)
+    result = merge_solutions(old_solutions, new_solutions, emerged_links, header)
     write_to_md(dst, result)
 
-if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2])
+#if __name__ == "__main__":
+main(r'C:\Users\savel\PycharmProjects\python_au\source_leetcode_data.txt', r'C:\Users\savel\PycharmProjects\python_au\leetcode\linked-list.md')
